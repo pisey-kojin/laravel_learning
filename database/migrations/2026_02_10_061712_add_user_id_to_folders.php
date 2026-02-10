99e98bd69d85dd5aceed36881b71b70a48ff50d3
sokpisey <sokpisey@sokpisey.local>
2026-02-10 15:15:50 +0700

    Prepare Login and Logout page

---
+++ database/migrations/2026_02_10_061712_add_user_id_to_folders.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     * Add user_id column into `folders` table
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('folders', function (Blueprint $table) {
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     * Delete 'user_id' column from 'folders' table
+     * 
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('folders', function (Blueprint $table) {
+            $table->dropColumn('user_id');
+        });
+    }
+};
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,11 +12,39 @@
     <header>
         <nav class="my-navbar">
             <a href="/" class="my-navbar-brand">ToDo App</a>
+            <div class="my-navbar-control">
+                @if (Auth::check())
+                    <span class="my-navbar-item">ようこそ、 {{ Auth::user()->name }}さん</span>
+                    |
+                    <a href="#" id="logout" class="my-navbar-item">ログアウト</a>
+                    <form id="logout-form" action="{{ route('logout') }}" method ="POST" style="display: none">
+                        @csrf
+                    </form>
+        
+                @else
+                    <a href="{{ route('login') }}" class="my-navbar-item">ログイン</a>
+                    |
+                    <a href="{{ route('register') }}" class="my-navbar-item">会員登録</a>
+                @endif
+            </div>
         </nav>
     </header>
     <main>
         @yield('content')
     </main>
     @yield('script')
+    <!--
+    *   ログアウトのクリックイベント
+    *   機能：ログアウトリンクのクリック時に真下のログアウトフォームを送信する
+    *   用途：ログアウトを実施する
+    -->
+    @if(Auth::check())
+        <script>
+            document.getElementById('logout').addEventListener('click', function(event) {
+            event.preventDefault();
+            document.getElementById('logout-form').submit();
+            });
+        </script>
+    @endif
 </body>
 </html>
