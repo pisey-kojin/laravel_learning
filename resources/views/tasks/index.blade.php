7a0193bf70ad822eefe6eccf3c66401a0981381d
sokpisey <sokpisey@sokpisey.local>
2026-02-11 08:39:54 +0700

    Connect User and Folder relationship

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -22,17 +22,20 @@
                     </div>
                     <div class="list-group">
                         <table class="table foler-table">
-                            @foreach($folders as $folder)
-                            <tr>
-                                <td>
-                                    <a href="{{ route('tasks.index', ['id' => $folder->id]) }}" class="list-group-item {{ $folder_id === $folder->id ? 'active' : ''}}">
-                                        {{ $folder->title }}
-                                    </a>
-                                </td>
-                                <td><a href="{{ route('folders.edit', ['id'=> $folder->id])}}">編集</a></td>
-                                <td><a href="{{ route('folders.delete', ['id' => $folder->id])}}">削除</a></td>
-                            </tr>
-                            @endforeach
+                            @foreach ($folders as $folder)
+                                @if ($folder->user_id === Auth::user()->id)
+                                    <tr>
+                                        <td>
+                                            <a href="{{ route('tasks.index', ['id' => $folder->id]) }}" class="list-group-item {{ $folder_id === $folder->id ? 'active' : ''}}">
+                                                {{ $folder->title }}
+                                            </a>
+                                        </td>
+                                        <td><a href="{{ route('folders.edit', ['id'=> $folder->id])}}">編集</a></td>
+                                        <td><a href="{{ route('folders.delete', ['id' => $folder->id])}}">削除</a></td>
+                                    </tr>
+                                @endif
+                                
+                            @endforeach    
                         </table>
                     </div>
                 </nav>
